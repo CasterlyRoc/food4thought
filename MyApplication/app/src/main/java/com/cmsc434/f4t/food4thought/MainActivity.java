21f02f769068c082ef83ec9e8b91dbda01d662fa
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-11-22 19:13:38 -0500

    Project files added

---
+++ MyApplication/app/src/main/java/com/cmsc434/f4t/food4thought/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cmsc434.f4t.food4thought;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
