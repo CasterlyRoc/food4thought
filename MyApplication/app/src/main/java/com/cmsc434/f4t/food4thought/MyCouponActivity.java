8ec56826d9f117519af78a97b4983b21197adc84
adeseyeafe <adeseyeafe@github.com>
2015-12-07 18:36:05 -0500

    more stuff

---
+++ MyApplication/app/src/main/java/com/cmsc434/f4t/food4thought/MyCouponActivity.java
@@ -0,0 +1,95 @@
+package com.cmsc434.f4t.food4thought;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Button;
+
+public class MyCouponActivity extends MyCouponListActivity {
+		String passedBusiness;
+		String passedTitle;
+		String passedDescription;
+		String passedCost;
+        private ImageView passedViewImage = null;
+		private TextView passedViewBusi = null;
+		private TextView passedViewTitle = null;
+		private TextView passedViewDesc = null;
+
+		Button use;
+        AlertDialog dialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_coupon);
+				
+        use = (Button) findViewById(R.id.button);
+
+        Bundle extras = getIntent().getExtras();
+        passedBusiness = extras.getString(business);
+        passedTitle = extras.getString(title);
+        passedDescription = extras.getString(description);
+        passedCost = extras.getString(cost);
+
+        passedViewImage = (ImageView) findViewById(R.id.image);
+        passedViewBusi = (TextView) findViewById(R.id.business);
+        passedViewTitle = (TextView) findViewById(R.id.title);
+        passedViewDesc = (TextView) findViewById(R.id.description);
+
+        if (passedBusiness.equals("McDonalds")){
+            passedViewImage.setImageResource(R.drawable.mcdonalds_logo);
+        } else if (passedBusiness.equals("Taco Bell")){
+            passedViewImage.setImageResource(R.drawable.taco_bell_logo);
+        } else if (passedBusiness.equals("Chipotle")){
+            passedViewImage.setImageResource(R.drawable.chipotle_logo);
+        }
+        passedViewBusi.setText(passedBusiness);
+        passedViewTitle.setText(passedTitle);
+        passedViewDesc.setText(passedDescription);
+
+        /*use.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder popup = new AlertDialog.Builder(
+                        MyCouponActivity.this);
+                popup.setTitle("Use this Coupon?");
+
+                // PositiveButton, deletes current position.
+                popup.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                int deletePos = MyCouponListActivity.deletePos;
+<<<<<<< HEAD
+								MyCouponListActivity.dataAdapter.remove(dataAdapter.getItem(deletePos));
+=======
+                                MyCouponListActivity.dataAdapter.remove(dataAdapter.getItem(deletePos));
+>>>>>>> 40f29324b370262abf19c785fcc4f1b0e917b8d7
+                                MyCouponListActivity.dataAdapter.notifyDataSetChanged();
+                                finish();
+                            }
+                        });
+
+                // NegativeButton, closes the pop-up.
+                popup.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                    int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                dialog = popup.create();
+                dialog.show();
+            }
+        });*/
+    }
+}
