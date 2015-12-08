f5d2a9506ae9a9117ba70a3e3fc006faa687e754
Casterly_Roc <zknopp@terpmail.umd.edu>
2015-12-08 01:16:21 -0500

    Test

--- MyApplication/app/src/main/java/com/cmsc434/f4t/food4thought/BuyCoupons.java
+++ MyApplication/app/src/main/java/com/cmsc434/f4t/food4thought/BuyCoupons.java
@@ -1,11 +1,18 @@
 package com.cmsc434.f4t.food4thought;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,17 +21,12 @@ import java.util.List;
 public class BuyCoupons extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.cmsc434.food4thought.MESSAGE";
+    ArrayAdapter<Button> dataAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_coupons);
-        displayViewMcdonalds();
-    }
-
-    public void updatePoints(View view){
-        TextView text = (TextView) findViewById(R.id.points);
-        text.setText("Test");
     }
 
     public void goToIndCoupon(View view){
@@ -34,8 +36,5 @@ public class BuyCoupons extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
-    private void displayViewMcdonalds(){
-        List<Button> couponList = new ArrayList<>();
-        Button button1 = new Button(this);
-    }
+
 }
